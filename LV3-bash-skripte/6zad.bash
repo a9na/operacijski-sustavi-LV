a9a0ab89832e4075b0ecc18eeb4d16a33f265908
Ana Novković <anaiscoding@gmail.com>
2024-03-26 08:38:41 +0100

    Create 6zad.bash

---
+++ LV3-bash-skripte/6zad.bash
@@ -0,0 +1,17 @@
+#Skripta za provjeru palindroma:
+
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <string>"
+    exit 1
+fi
+
+string=$1
+reverse_string=$(echo "$string" | rev)
+
+if [ "$string" == "$reverse_string" ]; then
+    echo "$string is a palindrome"
+else
+    echo "$string is not a palindrome"
+fi
