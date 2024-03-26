0f3885a67c9010041090237723838eed320122c0
Ana Novković <anaiscoding@gmail.com>
2024-03-26 08:25:12 +0100

    Create 1zad.bash

---
+++ LV3-bash-skripte/1zad.bash
@@ -0,0 +1,14 @@
+#Skripta za ispisivanje linija između određenih brojeva linija:
+
+#!/bin/bash
+
+if [ $# -ne 3 ]; then
+    echo "Usage: $0 <filename> <start_line> <end_line>"
+    exit 1
+fi
+
+filename=$1
+start_line=$2
+end_line=$3
+
+sed -n "${start_line},${end_line}p" "$filename"
